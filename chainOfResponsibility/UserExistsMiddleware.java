1c5bb9aae2526de565ca984b701c118ec59eb5cf
Adam <adam.mashrabov@lovet.co.kr>
2025-03-22 16:19:49 +0900

    chain: add user exists check middleware

---
+++ chainOfResponsibility/UserExistsMiddleware.java
@@ -0,0 +1,27 @@
+package chainOfResponsibility;
+
+/**
+ * UserExistsMiddleware class is a concrete middleware class that checks
+ * if the user exists in the server. It extends the Middleware class and
+ * implements the check method.
+ */
+public class UserExistsMiddleware extends Middleware {
+    Server server;
+
+    public UserExistsMiddleware(Server server) {
+        this.server = server;
+    }
+
+    public boolean check(String email, String password) {
+        if (!server.hasEmail(email)) {
+            System.out.println("This email is not registered!");
+            return false;
+        }
+        if (!server.isValidPassword(email, password)) {
+            System.out.println("Wrong password!");
+            return false;
+        }
+        return checkNext(email, password);
+    }
+
+}
