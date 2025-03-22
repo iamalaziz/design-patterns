c7ce049ae98f8b2e4fe2956cbbb3198a3f53fc5e
Adam <adam.mashrabov@lovet.co.kr>
2025-03-22 16:19:09 +0900

    chain: add base middleware

---
+++ chainOfResponsibility/Middleware.java
@@ -0,0 +1,36 @@
+package chainOfResponsibility;
+
+/**
+ * The Middleware class represents a middleware component in the chain of
+ * responsibility.
+ */
+public abstract class Middleware {
+    private Middleware next;
+
+    public static Middleware link(Middleware first, Middleware... chain) {
+        Middleware head = first;
+        for (Middleware nextInChain : chain) {
+            head.next = nextInChain;
+            head = nextInChain;
+        }
+        return first;
+    }
+
+    /**
+     * The method for setting the next middleware in the chain.
+     * Subclasses will implement this method with concrete checks.
+     */
+    public abstract boolean check(String email, String password);
+
+    /**
+     * Checks the next middleware in the chain, if it exists.
+     * If there is no next middleware, the check passes by default.
+     */
+    protected boolean checkNext(String email, String password) {
+        if (next == null) {
+            return true;
+        }
+
+        return next.check(email, password);
+    }
+}
